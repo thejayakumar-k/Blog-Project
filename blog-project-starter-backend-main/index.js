dc8d97997e218b16e9e1b4ca81c9df35f1c213ba
JAYAKUMAR K <kjaya3331@gmail.com>
2026-07-28 17:23:12 +0530

    fix cors error

--- blog-project-starter-backend-main/index.js
+++ blog-project-starter-backend-main/index.js
@@ -8,13 +8,21 @@ const cors = require("cors");
 const crypto = require("crypto");
 const admin = require("firebase-admin");
 const { getAuth } = require("firebase-admin/auth");
-const serviceAccount = require("./serviceAccountKey.json");
 require("dotenv").config();
 
-admin.initializeApp({
-  credential: admin.cert(serviceAccount),
-});
-
+if (!admin.apps.length) {
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    admin.initializeApp({
+      credential: admin.cert(serviceAccount),
+    });
+  } else {
+    const serviceAccount = require("./serviceAccountKey.json");
+    admin.initializeApp({
+      credential: admin.cert(serviceAccount),
+    });
+  }
+}
 const app = express();
 
 app.use(cors({
