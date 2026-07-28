b9404b3bca0ab9842f67dd4bbf583719e94e2af2
JAYAKUMAR K <kjaya3331@gmail.com>
2026-07-28 17:29:23 +0530

    fix: update Firebase env configuration and prevent Vercel DNS startup crash

--- blog-project-starter-backend-main/index.js
+++ blog-project-starter-backend-main/index.js
@@ -1,6 +1,8 @@
-const dns = require("dns");
-dns.setDefaultResultOrder("ipv4first");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+try {
+  const dns = require("dns");
+  dns.setDefaultResultOrder("ipv4first");
+  dns.setServers(["8.8.8.8", "8.8.4.4"]);
+} catch (e) {}
 
 const express = require("express");
 const mongoose = require("mongoose");
